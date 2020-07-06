542a9c2cfaf5430efe06f54092f1adb6fe8b80c9
Hamada, Yota <yota.hamada@rakuten.com>
2020-07-06 09:27:27 +0900

    a

--- tickets/auth/src/index.ts
+++ tickets/auth/src/index.ts
@@ -4,10 +4,6 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.get('/', (req, res) => {
-  res.send("hello")
-})
-
 app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+  console.log('Listening on port 3000!!');
 });
