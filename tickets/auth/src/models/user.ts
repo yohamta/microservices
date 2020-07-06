a3f1ad6a724221938e797c459a61526f5d4acc27
Hamada, Yota <yota.hamada@rakuten.com>
2020-07-07 05:24:22 +0900

    a

---
+++ tickets/auth/src/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<any> {
+  build(atrrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
+
+export { User };
