d9ad240f11187b3f38e8810007db8de053825838
Hamada, Yota <yota.hamada@rakuten.com>
2020-07-07 18:22:24 +0900

    a

--- tickets/auth/src/routes/signin.ts
+++ tickets/auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("hello");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send("login");
+  }
+);
 
 export { router as signinRouter };
