f675754af3ef1b34fa6e5cdbb5f7c8d56875fb0a
Hamada, Yota <yota.hamada@rakuten.com>
2020-07-07 18:39:48 +0900

    a

--- tickets/auth/src/routes/signup.ts
+++ tickets/auth/src/routes/signup.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
+
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 
