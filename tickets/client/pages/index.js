aa06a7c4d9741b199da9f1c01fcbc30aef7d98ec
Hamada, Yota <yota.hamada@rakuten.com>
2020-07-10 16:03:43 +0900

    a

--- tickets/client/pages/index.js
+++ tickets/client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import axios from "axios";
+
+const LP = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page: {currentUser.email}</h1>;
 };
+
+LP.getInitialProps = async ({ req }) => {
+  const response = await axios.get(
+    "http://auth-srv:3000/api/users/currentuser",
+    { headers: req ? { cookie: req.headers.cookie } : null }
+  );
+  return response.data;
+};
+
+export default LP;
