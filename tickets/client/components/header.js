3d9e5f4417f22b384d06334da22bc1f9da413c3d
Hamada, Yota <yota.hamada@rakuten.com>
2020-07-10 18:30:48 +0900

    a

--- tickets/client/components/header.js
+++ tickets/client/components/header.js
@@ -1,5 +1,19 @@
 import Link from "next/link";
 const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => (
+      <li key={href} className="nav-item">
+        <Link href={href}>
+          <a className="navbar-brand">{label}</a>
+        </Link>
+      </li>
+    ));
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
@@ -7,9 +21,7 @@ const Header = ({ currentUser }) => {
       </Link>
 
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign out" : "Sign in"}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
