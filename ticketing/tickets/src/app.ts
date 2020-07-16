3ae90260056a729ecc66e5320f3ebc6a394eed5b
Hamada, Yota <yota.hamada@rakuten.com>
2020-07-16 16:30:24 +0900

    a

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@yotahamada/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.get("*", async (_req, _res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
