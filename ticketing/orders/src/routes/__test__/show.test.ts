03087d2fc808adb9547d462f68c590a5bef2db59
Yota Hamada <yohamta@gmail.com>
2020-08-09 17:35:08 +0900

    a

--- ticketing/orders/src/routes/__test__/show.test.ts
+++ ticketing/orders/src/routes/__test__/show.test.ts
@@ -28,3 +28,28 @@ it("fetches the order", async () => {
 
   expect(fetchedOrder.id).toEqual(order.id);
 });
+
+it("return an error if one user tries to fetch another users order", async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signup();
+
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", global.signup())
+    .send()
+    .expect(401);
+});
