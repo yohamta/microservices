47aead0f4d41b67de00db3d77a2caddd2dfcf134
Yota Hamada <yohamta@gmail.com>
2020-08-09 17:56:29 +0900

    a

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,38 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
+} from "@yotahamada/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // TODO: Publishing an event saying this was cancelled
+
+    // The 204 response must not include content
+    res.status(204).send();
+  }
+);
 
 export { router as deleteOrderRouter };
