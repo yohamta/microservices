314f8c10e1971a3e06d4d029b9c55776bdabbbb7
Yota Hamada <yohamta@gmail.com>
2020-08-13 23:04:21 +0900

    a

---
+++ ticketing/orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,80 @@
+import { TicketCreatedListener } from "../ticket-created-listener";
+import { Message } from "node-nats-streaming";
+import mongoose from "mongoose";
+import { natsWrapper } from "../../../nats-wrapper";
+import {
+  TicketCreatedEvent,
+  OrderStatus,
+  ExpirationCompleteEvent,
+} from "@yotahamada/common";
+import { Ticket } from "../../../models/ticket";
+import { Order } from "../../../models/order";
+import { ExpirationCompleteListener } from "../expiration-complete-listener";
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create and save a order
+  const order = Order.build({
+    userId: "123",
+    expireAt: new Date(),
+    status: OrderStatus.Created,
+    ticket,
+  });
+  await order.save();
+
+  // create a fake data event
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // create a fake message obect
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("updates the order status to cancelled", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.orderId);
+  expect(order!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it("emits an OrderCancelled event", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const eventData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(eventData.id).toEqual(data.orderId);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -7,6 +7,7 @@ import {
 } from "@yotahamada/common";
 import { queueGroupName } from "./queue-group-name";
 import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
 export class ExpirationCompleteListener extends Listener<
   ExpirationCompleteEvent
@@ -15,7 +16,7 @@ export class ExpirationCompleteListener extends Listener<
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate("ticket");
 
     if (!order) {
       throw new Error("Order not found");
@@ -23,6 +24,14 @@ export class ExpirationCompleteListener extends Listener<
 
     order.status = OrderStatus.Cancelled;
     await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     msg.ack();
   }
