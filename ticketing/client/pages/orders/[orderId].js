c64072f59b6ce041efeca9882623c2cf9fa112f4
Yota Hamada <yohamta@gmail.com>
2020-08-17 17:32:08 +0900

    a

--- ticketing/client/pages/orders/[orderId].js
+++ ticketing/client/pages/orders/[orderId].js
@@ -1,6 +1,8 @@
-import useRequest from "../../hooks/use-request";
 import { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import Router from "next/router";
+
+import useRequest from "../../hooks/use-request";
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
@@ -11,7 +13,7 @@ const OrderShow = ({ order, currentUser }) => {
     body: {
       orderId: order.id,
     },
-    onSuccess: (payment) => console.log(payment),
+    onSuccess: (payment) => Router.push("/orders"),
   });
 
   useEffect(() => {
---
+++ ticketing/client/pages/orders/index.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const OrderIndex = ({ orders }) => {
+  return (
+    <ul>
+      {orders.map((order) => {
+        return (
+          <li key={order.id}>
+            {order.ticket.title} - {order.status}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+OrderIndex.getInitialProps = async (context, client) => {
+  const { data } = await client.get("/api/orders");
+
+  return { orders: data };
+};
+
+export default OrderIndex;
